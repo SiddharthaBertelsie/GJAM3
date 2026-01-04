90180e8056d7fc87de64aeef95db886105e37884
Banjo - Home Laptop <p.boonasuk@gmail.com>
2026-01-05 09:30:12 +1100

    Unity EXE Build Made
    Unfortunatley hadno space to quickly get WebGL compilier, so had to do with the EXE

--- Assets/GAD176EnemyAI/EnemyAIInterfaces/IFleeFunctionality.cs
+++ Assets/GAD176EnemyAI/EnemyAIInterfaces/IFleeFunctionality.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro.EditorUtilities;
 using UnityEngine;
 
 namespace SAE.GAD176.P1.EnemyAI
--- Assets/GAD176EnemyAI/PlayerSightedChecker.cs
+++ Assets/GAD176EnemyAI/PlayerSightedChecker.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UIElements;
 using UnityEngine;
 using GJAM3.Enemy;
 
